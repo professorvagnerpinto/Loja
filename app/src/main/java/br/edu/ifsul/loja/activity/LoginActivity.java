94b008d56e0041e942e8662fad8cc3d64ba0a8fb
vagner <professor.vagnerpinto@gmail.com>
2019-08-23 11:57:54 -0300

    Reset de senha e finalização do login.

--- app/src/main/java/br/edu/ifsul/loja/activity/LoginActivity.java
+++ app/src/main/java/br/edu/ifsul/loja/activity/LoginActivity.java
@@ -1,11 +1,14 @@
 package br.edu.ifsul.loja.activity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,18 +21,16 @@ import com.google.firebase.auth.FirebaseUser;
 
 import br.edu.ifsul.loja.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "mainActivity";
     private EditText etEmail, etSenha;
-    private Button btLogar;
-    private TextView tvEsqueceuSenha;
     private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
@@ -37,10 +38,9 @@ public class MainActivity extends AppCompatActivity {
         //mapeia os componentes da UI
         etEmail = findViewById(R.id.etEmail_login);
         etSenha = findViewById(R.id.etSenha_login);
-        btLogar = findViewById(R.id.btLogar_login);
-        tvEsqueceuSenha = findViewById(R.id.tv_esqueceusenha_login);
 
-        btLogar.setOnClickListener(new View.OnClickListener() {
+        //trata evento onclick do button
+        findViewById(R.id.btLogar_login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = etEmail.getText().toString();
@@ -49,20 +49,21 @@ public class MainActivity extends AppCompatActivity {
                 if(!email.isEmpty() && !senha.isEmpty()){
                     signIn(email, senha);
                 }else{
-                    Toast.makeText(MainActivity.this, getString(R.string.toast_preencher_todos_campos), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, getString(R.string.toast_preencher_todos_campos), Toast.LENGTH_SHORT).show();
                 }
             }
         });
-        
-        tvEsqueceuSenha.setOnClickListener(new View.OnClickListener() {
+
+        //trata evento onclick do textview
+        findViewById(R.id.tv_esqueceusenha_login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = etEmail.getText().toString();
                 String senha = etSenha.getText().toString();
                 if(!email.isEmpty() && !senha.isEmpty()){
-                    resetarSenha(email, senha);
+                    resetarSenha(email);
                 }else{
-                    Toast.makeText(MainActivity.this, getString(R.string.toast_preencher_todos_campos), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(LoginActivity.this, getString(R.string.toast_preencher_todos_campos), Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -77,20 +78,46 @@ public class MainActivity extends AppCompatActivity {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            //updateUI(user);
+                            startActivity(new Intent(LoginActivity.this,
+                                    ProdutosActivity.class));
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            Toast.makeText(MainActivity.this, getString(R.string.toast_falha_autenticacao),
+                            Toast.makeText(LoginActivity.this, getString(R.string.toast_falha_autenticacao),
                                     Toast.LENGTH_SHORT).show();
                             //updateUI(null);
                         }
-
-                        // ...
                     }
                 });
     }
 
-    private void resetarSenha(String email, String senha) {
+    private void resetarSenha(final String email) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //add the title and text
+        builder.setTitle("Atenção");
+        builder.setMessage("Um email de recuperação de senha será enviado para: " + email);
+        //add the buttons
+        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "Email sent.");
+                                }
+                            }
+                        });
+
+            }});
+        builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Snackbar.make(findViewById(R.id.R_id_container_activity_login), "Operação cancelada.", Snackbar.LENGTH_LONG).show();
+                }
+            });
+
+        builder.show();
     }
 }
