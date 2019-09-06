b23289533f11719d6be406f2f54abfcc37278629
vagner <professor.vagnerpinto@gmail.com>
2019-09-06 11:58:00 -0300

    Construção do adapter.

--- app/src/main/java/br/edu/ifsul/loja/activity/ProdutosActivity.java
+++ app/src/main/java/br/edu/ifsul/loja/activity/ProdutosActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.widget.ListView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -13,8 +12,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.edu.ifsul.loja.R;
 import br.edu.ifsul.loja.adapter.ProdutosAdapter;
+import br.edu.ifsul.loja.model.Produto;
 
 public class ProdutosActivity extends AppCompatActivity {
 
@@ -36,9 +39,17 @@ public class ProdutosActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Log.d(TAG, "dataSnapshot=" + dataSnapshot);
+                List<Produto> listProdutos = new ArrayList<>();
                 for(DataSnapshot ds : dataSnapshot.getChildren()){
                     Log.d(TAG, "dataSnapshot=" + ds);
+                    Produto produto = ds.getValue(Produto.class);
+                    produto.setKey(ds.getKey());
+                    produto.setIndex(listProdutos.size());
+                    listProdutos.add(produto);
                 }
+
+                //faz o bindView
+                lvProdutos.setAdapter(new ProdutosAdapter(this, listProdutos));
             }
 
             @Override
@@ -46,10 +57,6 @@ public class ProdutosActivity extends AppCompatActivity {
 
             }
         });
-
-
-        //faz o bindView
-        //lvProdutos.setAdapter(new ProdutosAdapter());
 
     }
 }
